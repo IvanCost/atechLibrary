376a351c1a6ffae694247c831bbc23d47c7c0fda
ivan <ivancosta@usp.br>
2019-11-14 13:00:06 -0300

    grid view

---
+++ library/src/main/java/com/atech/library/GridViewCustom.java
@@ -0,0 +1,14 @@
+package com.atech.library;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GridViewCustom extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid_view_custom);
+    }
+}
